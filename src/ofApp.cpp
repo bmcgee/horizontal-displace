ac2ea73cef7f2315f975341123f085e802808746
Brian McGee <brian.mcgee@vice.com>
2018-07-12 18:48:39 -0400

    Initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,108 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofBackground(ofColor::white);
+	
+	imageMap.load("joel2.png");
+	imageMap.resize(100,100);
+	
+	cols = 100;
+	rows = 100;
+	height = 1000;
+	width = 1000;
+	
+	int xScalar = height / rows;
+	int yScalar = width / cols;
+	
+	for (int y=0; y<(rows); y++) {
+		ofPath path;
+		path.moveTo(0, y, 0);
+		for (int x=1; x<(cols); x++) {
+			//ofColor c = imageMap.getColor((x*xScalar),(y*yScalar));
+			ofColor c = imageMap.getColor(x,y);
+
+			float z = c.getLightness();
+			z = ofMap(z, 0, 255, 0, 10);
+			path.lineTo(x, y, (z));
+		}
+		path.setStrokeColor(ofColor::black);
+		path.setFillColor(ofColor::white);
+		path.setFilled(false);
+		path.setStrokeWidth(1);
+		paths.push_back(path);
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	cam.begin();
+	cam.setFarClip(1000);
+	cam.setNearClip(0);
+	for (int i=0; i < paths.size(); i++) {
+		paths[i].draw();
+	}
+	//path.draw();
+	cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
